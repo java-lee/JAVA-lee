f4865daf8ba7d330f83ee76c854bf507053d79ce
Thiago <Thiago@DESKTOP-DK3JA2R>
2019-06-13 16:42:41 -0300

    Primeira alteracao

---
+++ demo/src/main/java/ERP/JAVAlee/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package ERP.JAVAlee.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
