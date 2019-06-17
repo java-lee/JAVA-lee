cf6080f3d1980061d634047cbf56ed42ed7563a7
Thiago <Thiago@DESKTOP-DK3JA2R>
2019-06-17 00:59:52 -0300

    SpringbotImport1

--- demo/src/main/java/ERP/JAVAlee/demo/DemoApplication.java
+++ demo/src/main/java/ERP/JAVAlee/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package ERP.JAVAlee.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- demo/src/main/java/ERP/JAVAlee/demo/evento.java
+++ demo/src/main/java/ERP/JAVAlee/demo/evento.java
@@ -1,18 +1,21 @@
 package ERP.JAVAlee.demo;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class evento {
     
-    @RequestMapping("/")
+    /*@RequestMapping("/")
     public String teste(){
+    	System.out.println("Entrou no principal!!");
         return "teste";
-    }
+    }*/
     
     @RequestMapping("/inicial")
     public String inicial(){
-        return "inicial";
+    	System.out.println("Entrou no inicial!!");
+        return "PORQUE ESSA MERDA NAO FUNCIONA CARALHO!";
     }
 }
