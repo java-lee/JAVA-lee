4c790cf49e3dc6f4bfca0c73aee64650e368556b
Thiago <Thiago@DESKTOP-DK3JA2R>
2019-06-19 02:56:04 -0300

    Integracao com facebook, tela de login e cadastro

--- demo/src/main/java/ERP/JAVAlee/demo/evento.java
+++ demo/src/main/java/ERP/JAVAlee/demo/evento.java
@@ -1,6 +1,8 @@
 package ERP.JAVAlee.demo;
 
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,10 +14,24 @@ public class evento {
     	System.out.println("Entrou no principal!!");
         return "paginaInicial.html";
     }
+	
+	@RequestMapping("/user/cadastrar")
+    public String cadastrar(){
+		System.out.println("Entrou no cadastrar!!");
+		return "cadastrar.html";
+    }
+	
+	@RequestMapping("/user/logar")
+    public String logar(){
+		System.out.println("Entrou no cadastrar!!");
+		return "logar.html";
+    }
     
     @RequestMapping("/inicial")
     public String inicial(){
     	System.out.println("Entrou no inicial!!");
         return "dataagr.jsp";
     }
+    
+    
 }
---
+++ demo/src/main/webapp/arquivosCss/ligarscss.bat
@@ -0,0 +1,1 @@
+sass --watch header.scss:header.css
